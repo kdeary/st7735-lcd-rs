012faf0d820a6590bb6b57626997fd55934224d6
Korbin Deary <korbin.deary45@gmail.com>
2024-04-12 14:34:16 -0400

    removed graphics

--- src/lib.rs
+++ src/lib.rs
@@ -245,96 +245,15 @@ where
         self.set_address_window(sx, sy, ex, ey)?;
         self.write_pixels_buffered(colors)
     }
-}
 
-#[cfg(feature = "graphics")]
-extern crate embedded_graphics_core;
-#[cfg(feature = "graphics")]
-use self::embedded_graphics_core::{
-    draw_target::DrawTarget,
-    pixelcolor::{
-        raw::{RawData, RawU16},
-        Rgb565,
-    },
-    prelude::*,
-    primitives::Rectangle,
-};
-
-#[cfg(feature = "graphics")]
-impl<SPI, DC, RST> DrawTarget for ST7735<SPI, DC, RST>
-where
-    SPI: spi::SpiDevice,
-    DC: OutputPin,
-    RST: OutputPin,
-{
-    type Error = ();
-    type Color = Rgb565;
-
-    fn draw_iter<I>(&mut self, pixels: I) -> Result<(), Self::Error>
-    where
-        I: IntoIterator<Item = Pixel<Self::Color>>,
-    {
-        for Pixel(coord, color) in pixels.into_iter() {
-            // Only draw pixels that would be on screen
-            if coord.x >= 0
-                && coord.y >= 0
-                && coord.x < self.width as i32
-                && coord.y < self.height as i32
-            {
-                self.set_pixel(
-                    coord.x as u16,
-                    coord.y as u16,
-                    RawU16::from(color).into_inner(),
-                )?;
-            }
-        }
-
-        Ok(())
-    }
-
-    fn fill_contiguous<I>(&mut self, area: &Rectangle, colors: I) -> Result<(), Self::Error>
-    where
-        I: IntoIterator<Item = Self::Color>,
-    {
-        // Clamp area to drawable part of the display target
-        let drawable_area = area.intersection(&Rectangle::new(Point::zero(), self.size()));
-
-        if drawable_area.size != Size::zero() {
-            self.set_pixels_buffered(
-                drawable_area.top_left.x as u16,
-                drawable_area.top_left.y as u16,
-                (drawable_area.top_left.x + (drawable_area.size.width - 1) as i32) as u16,
-                (drawable_area.top_left.y + (drawable_area.size.height - 1) as i32) as u16,
-                area.points()
-                    .zip(colors)
-                    .filter(|(pos, _color)| drawable_area.contains(*pos))
-                    .map(|(_pos, color)| RawU16::from(color).into_inner()),
-            )?;
-        }
-
-        Ok(())
-    }
-
-    fn clear(&mut self, color: Self::Color) -> Result<(), Self::Error> {
+    fn clear(&mut self, color: u16) -> Result<(), Self::Error> {
         self.set_pixels_buffered(
             0,
             0,
             self.width as u16 - 1,
             self.height as u16 - 1,
-            core::iter::repeat(RawU16::from(color).into_inner())
+            core::iter::repeat(color)
                 .take((self.width * self.height) as usize),
         )
-    }
-}
-
-#[cfg(feature = "graphics")]
-impl<SPI, DC, RST> OriginDimensions for ST7735<SPI, DC, RST>
-where
-    SPI: spi::SpiDevice,
-    DC: OutputPin,
-    RST: OutputPin,
-{
-    fn size(&self) -> Size {
-        Size::new(self.width, self.height)
     }
 }
